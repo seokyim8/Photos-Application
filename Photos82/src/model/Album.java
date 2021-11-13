3b53ec80cfa65a11b549fc3f7b2ff1cafaa81180
Seok Yim(Noah) <seokyim8@gmail.com>
2021-11-13 00:53:34 -0500

    Modified Album.java and Photo.java. Still need to make changes to Album.java.

--- Photos82/src/model/Album.java
+++ Photos82/src/model/Album.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Album {
@@ -15,10 +16,51 @@ public class Album {
 		this.photos = new ArrayList<Photo>();
 	}
 	
-	public void addPhoto() {
+	/**
+	 * Adds photo to the Album instance.
+	 * 
+	 * @param filePath
+	 * @return	0 if successful, 1 if filePath was invalid, 2 if photo already exists 
+	 * @throws IOException 
+	 */
+	public int addPhoto(String filePath) throws IOException {//need to apply change to date_range
+		for(int i = 0; i < this.photos.size(); i++) {
+			if(this.photos.get(i).filePath.compareTo(filePath)== 0) {
+				return 2;
+			}
+		}
+		Photo photo = Photo.createPhoto(filePath, this);
+		if(photo == null) {
+			return 1;
+		}
 		
+		this.photos.add(photo);
+		this.num_of_photos++;
+		
+		//apply change to date_range
+		
+		return 0;
 	}
-	public void deletePhoto() {
-		
+	public boolean deletePhoto(String filePath) {//need to apply change to date_range
+		for(int i = 0; i < this.photos.size(); i++) {
+			if(this.photos.get(i).filePath.compareTo(filePath)== 0) {
+				this.photos.remove(i);
+				this.num_of_photos--;
+				//apply change to date_range
+
+				
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean rename(String name) {
+		for(int i = 0; i < this.user.albums.size(); i++) {
+			if(this.user.albums.get(i).name.compareTo(name) == 0) {
+				return false;
+			}
+		}
+		this.name = name;
+		return true;
 	}
 }
--- Photos82/src/model/Photo.java
+++ Photos82/src/model/Photo.java
@@ -1,24 +1,35 @@
 package model;
 
-import java.time.LocalDate;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 
 public class Photo {
 	String filePath;
 	String caption;
-	LocalDate date;
+	LocalDateTime datetime;
 	ArrayList<Album> albums;
 	ArrayList<Tag> tags;
 	
-	public Photo(String filePath, Album album) {
+	private Photo(String filePath, Album album) throws IOException {
 		this.filePath = filePath;
-		ZoneId zid = ZoneId.of("America/New_York");
-		this.date = LocalDate.now(zid);
+		FileTime filetime = Files.getLastModifiedTime(new File(filePath).toPath());
+		this.datetime = LocalDateTime.ofInstant(filetime.toInstant(), ZoneId.of("America/New_York"));
 		this.albums = new ArrayList<Album>();
 		this.tags = new ArrayList<Tag>();
 		this.caption = "";
 		this.albums.add(album);
+	}
+	public static Photo createPhoto(String filePath, Album album) throws IOException {
+		File file = new File(filePath);
+		if(file.exists()) {
+			return new Photo(filePath,album);
+		}
+		return null;
 	}
 	public boolean addTag(String name, String value) {
 		for(int i = 0; i < this.tags.size(); i++) {
@@ -30,5 +41,18 @@ public class Photo {
 		Tag tag = new Tag(name,value,this);
 		this.tags.add(tag);
 		return true;
+	}
+	public boolean deleteTag(String name, String value) {
+		for(int i = 0; i < this.tags.size(); i++) {
+			if(this.tags.get(i).name.compareTo(name) == 0 
+					&& this.tags.get(i).value.compareTo(value) == 0) {
+				this.tags.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	public void modifyCaption(String caption) {
+		this.caption = caption;
 	}
 }
